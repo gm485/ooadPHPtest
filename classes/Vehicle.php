c05517c55bec2e7b85b5190cee052f5a388c9173
gm485 <b00156485@mytudublin.ie>
2024-03-13 15:20:49 +0000

    creatingCars

---
+++ classes/Vehicle.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace classes;
+
+class Vehicle
+{
+    private String $vehicleVin;
+    private String $make;
+    private String $model;
+    private String $year;
+    private float $mileage;
+}
